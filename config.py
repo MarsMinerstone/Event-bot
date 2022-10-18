3dbc6f4c46c2f5a8056441f330a30950c903285e
Ilya <58552731+MarsMinerstone@users.noreply.github.com>
2022-10-18 20:19:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@
 # import os
 
 #BOT_TOKEN = os.environ.get("blagodatbot_token")
-BOT_TOKEN = "5372916998:AAF4Vt0-98XnSMnrVCHmYrHFWLB-ie4m8kg"
-ADMIN = 466411276
+BOT_TOKEN = ""
+ADMIN = 
 # ADMIN = 103875278
 
-YOUTOKEN = "381764678:TEST:39744"
+YOUTOKEN = ""
