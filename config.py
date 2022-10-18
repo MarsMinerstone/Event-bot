fff975096eecdff9f9781d050e7849f9cd6e3924
Ilya <58552731+MarsMinerstone@users.noreply.github.com>
2022-10-18 20:11:22 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ import datetime
 import os
 
 #BOT_TOKEN = os.environ.get("blagodatbot_token")
-BOT_TOKEN = "5372916998:AAF4Vt0-98XnSMnrVCHmYrHFWLB-ie4m8kg"
-ADMIN = 466411276
+BOT_TOKEN = ""
+ADMIN = 
